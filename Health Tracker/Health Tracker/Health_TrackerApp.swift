afbc833d647d10eed34f706df2dce17120cbdd22
Sed Cash <builtbysed@gmail.com>
2025-05-11 10:38:10 -0700

    Add initial iOS project files

---
+++ Health Tracker/Health Tracker/Health_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Health_TrackerApp.swift
+//  Health Tracker
+//
+//  Created by Sed Cash on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Health_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
